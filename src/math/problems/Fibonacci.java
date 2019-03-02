c9423728e9835e1ebec7d66480a82a6f805d897c
kokilaz <kokilaz@gmail.com>
2019-03-02 17:24:10 -0500

    Fibonacci completed

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,6 +6,13 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+         int x = 0;
+         int y = 1;
+         for(int i=0; i<10; i++){
+             System.out.println(x);
+             x=x+y;
+             y=x-y;
+         }
 
 
     }
